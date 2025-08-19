d2565fc5b1aa42d32a164711e905511618b32c68
Dhanrajsuthar21 <dhanrajsuthar298@gmail.com>
2025-08-19 12:22:07 +0530

    Uploading main.js
    Uploading main.js

---
+++ main.js
@@ -0,0 +1,289 @@
+const courseData = {
+  "Information Technology": {
+    "Paid": [
+    {
+      name: "CS50's Introduction to Computer Science",
+      provider: "Harvard (EDX)",
+      duration: "6–18 hours per week",
+      discription: "This globally renowned course is Harvard's entry-level introduction to computer science. It covers core topics such as algorithms, data structures, software engineering, and web development using languages like C, Python, SQL, and JavaScript. Designed for both absolute beginners and those seeking a rigorous foundation, CS50 emphasizes problem-solving and computational thinking. It’s the perfect starting point for aspiring programmers, engineers, and tech professionals.",
+      image: "https://ccitraining.edu/wp-content/uploads/2023/12/Information-Technology.jpg",
+      link: "https://www.edx.org/learn/computer-science/harvard-university-cs50-s-introduction-to-computer-science"
+    },
+    {
+      name: "Introduction to Cyber Security",
+      provider: "Udemy",
+      duration: "1 hour 8 min",
+      discription: "Gain essential knowledge of how to protect systems and data from cyber threats. This course explains how cyber attacks work, introduces security policies and encryption, and teaches how individuals and organizations can defend themselves. Ideal for beginners, students, and professionals looking to understand basic cybersecurity principles in today’s digital world.",
+      image: "https://static.vecteezy.com/system/resources/thumbnails/006/950/568/small_2x/cyber-security-data-protection-business-concept-on-virtual-screen-shield-protect-icon-in-wireframe-robotic-hand-antivirus-interface-illustration-vector.jpg",
+      link: "https://www.udemy.com/course/certified-secure-netizen/"
+    }],
+    "Free": [
+    {
+      name: "Web Development Basics",
+      provider: "EDX",
+      duration: "1 hour",
+      discription: "This course introduces HTML, CSS, and JavaScript—the three pillars of web development. Learn to create simple web pages, style them, and add interactivity. It’s a hands-on, project-based course ideal for students or hobbyists aiming to build websites or kickstart a career in web development.",
+      image: "https://i.ibb.co/6RzCcwpN/1-f-Hr-AZJ1-L0-Ff9dv-Vex-L5-A.png",
+      link: "https://www.edx.org/learn/computer-science/ibm-guided-project-web-development-w-html-css-for-beginners"
+    },
+    {
+      name: "Google IT Automation with Python",
+      provider: "Udemy",
+      duration: "2 hour",
+      discription: "Tailored for those familiar with basic IT concepts, this course teaches Python programming to automate routine system administration tasks. You'll learn how to use Python for working with files, APIs, and managing IT resources. A practical course for IT support professionals seeking to enhance efficiency through automation.",
+      image: "https://fabiopacifici.com/wp-content/uploads/2020/02/Learn_python_with_google_image.jpg",
+      link: "https://www.udemy.com/course/python-from-beginner-to-expert-starter-free/"
+    }]
+  },
+  
+  "Business & Entrepreneurship": {
+    "Paid": [
+    {
+      name: "Entrepreneurship in Emerging Economies",
+      provider: "EDX",
+      duration: "18 hours",
+      discription: "Explore how entrepreneurship can solve complex social and business challenges in developing economies. This course provides real-world case studies from Latin America, Africa, and Asia to help learners understand opportunity recognition, innovation, and scalable impact. Perfect for entrepreneurs and changemakers in the global south.",
+      image: "https://grow.google/intl/uk/google-career-certificates/digital-marketing-ecommerce/",
+      link: "https://www.edx.org/learn/entrepreneurship/harvard-university-entrepreneurship-in-emerging-economies?index=product&queryID=21417bef07b01d1e4db67bdf56f80eee&position=4&linked_from=autocomplete&c=autocomplete"
+    },
+    {
+      name: "Innovation for Entrepreneurs",
+      provider: "Udemy",
+      duration: "1 hour",
+      discription: "Designed for current or aspiring entrepreneurs, this course explores how to turn innovative ideas into successful startups. Learn strategies for ideation, market validation, design thinking, and business model development. A valuable resource for anyone launching a business or working in a startup ecosystem.",
+      image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRVVLqGASMUSkTAdxM10lNP1QXEqxpsX6U1hw&s",
+      link: "https://www.udemy.com/course/60-day-startup-introduction-to-startups/"
+    }],
+    "Free": [{
+      name: "Fundamentals of Digital Marketing",
+      provider: "Udemy",
+      duration: "1 hour",
+      discription: "A comprehensive, certified course covering SEO, content marketing, analytics, email campaigns, and more. With practical tips and quizzes, this course helps learners master online tools to grow any business or personal brand. Ideal for entrepreneurs, job seekers, and students.",
+      image: "https://www.springboard.com/blog/wp-content/uploads/2022/06/what-is-digital-marketing.png",
+      link: "https://www.udemy.com/course/the-fundamentals-of-the-digital-marketing/"
+    },
+    {
+      name: "Business Fundamentals",
+      provider: "EDX",
+      duration: "18 hours",
+      discription: "Understand core business principles including accounting, marketing, finance, and strategy. Great for non-business majors, early-career professionals, or anyone looking to grasp how businesses operate in a competitive environment.",
+      image: "https://online.hbs.edu/Style%20Library/api/resize.aspx?imgpath=/PublishingImages/Business%20Fundamentals.jpg&w=1200&h=630",
+      link: "https://www.edx.org/learn/business-administration/university-of-british-columbia-business-foundations?index=product&queryID=391d5f0e1c09959a429b133c779c9890&position=1&results_level=first-level-results&term=business+fundamentals&objectID=course-1c30e9b3-78ee-4c57-9af9-922153073d99&campaign=Business+Foundations&source=edX&product_category=course&placement_url=https%3A%2F%2Fwww.edx.org%2Fsearch"
+    }]
+  },
+  
+  "Healthcare & Wellness": {
+    "Paid": [
+    {
+      name: "Introduction to Psychology",
+      provider: "EDX",
+      duration: "50 hours",
+      discription: "This engaging course explores human behavior, brain function, emotions, memory, and mental health through scientific psychology. Designed for anyone interested in how the mind works, it blends academic theory with everyday examples. Perfect for students, educators, and the curious learner.",
+      image: "https://blogs.longwood.edu/edwinelleportfolio/files/2016/12/blog-examprep-061913.jpg",
+      link: "https://www.edx.org/learn/python/st-margarets-episcopal-school-introduction-to-psychology"
+    },
+    {
+      name: "COVID-19: Tackling the Novel Coronavirus",
+      provider: "Udemy",
+      duration: "2 hours",
+      discription: "This timely course provides scientific insights into the origins, spread, and control measures for COVID-19. Learn about virology, public health response, and outbreak dynamics. Ideal for healthcare workers, policy-makers, and the general public seeking evidence-based knowledge.",
+      image: "https://www.statnews.com/wp-content/uploads/2020/02/Coronavirus-CDC-645x645.jpg",
+      link: "https://www.udemy.com/course/mental-health-for-coping-with-stress-anxiety-covid-19/"
+    }],
+    "Free": [{
+      name: "Health Informatics on FHIR",
+      provider: "EDX",
+      duration: "50 hours",
+      discription: "Explore how modern health data systems use the FHIR (Fast Healthcare Interoperability Resources) standard to improve interoperability. Learn the technical and practical aspects of electronic health record systems, APIs, and healthcare data exchange. Best suited for health informaticians and developers.",
+      image: "https://www.onyxhealth.io/wp-content/uploads/2021/02/fhir-1.png",
+      link: "https://www.edx.org/learn/health-informatics/the-georgia-institute-of-technology-health-informatics-data-and-interoperability-standards"
+    },
+    {
+      name: "Nutrition and Health: Macronutrients and Overnutrition",
+      provider: "EDX",
+      duration: "52 hours",
+      discription: "This course explores the impact of macronutrients—carbohydrates, proteins, and fats—on human health. Learn about global obesity trends, metabolic processes, and diet-related diseases. A must for students of health sciences, nutritionists, and health-conscious individuals.",
+      image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ2ACuGHpVzK7aiL7VtielvqPBgBgacxh9uUQ&s",
+      link: "https://www.edx.org/learn/nutrition/wageningen-university-research-nutrition-and-health-macronutrients-and-overnutrition"
+    }]
+  },
+  
+  "Art & Humanities": {
+    "Paid": [
+    {
+      name: "Modern Art & Ideas",
+      provider: "Udemy",
+      duration: "1 hour",
+      discription: "Explore 20th and 21st-century art through thematic discussions on identity, society, and politics. This course connects artworks with global issues and includes rich visual content and interviews with artists. Ideal for art enthusiasts, educators, and creatives.",
+      image: "https://cdn.mos.cms.futurecdn.net/7YrobQvFFzw8aWsAUtoYXB-650-80.jpg.webp",
+      link: "https://www.udemy.com/course/art-fundamentals-in-one-hour/"
+    },
+    {
+      name: "Shakespeare’s Life and Work",
+      provider: "EDX",
+      duration: "20 hours",
+      discription: "Delve into the life, legacy, and literary genius of William Shakespeare. This course covers his plays, sonnets, and historical context. Whether you're a student or literature lover, it brings the Bard’s timeless works to life with academic depth and theatrical flair.",
+      image: "https://cdn.thecollector.com/wp-content/uploads/2024/08/top-plays-william-shakespeare.jpg?width=1400&quality=70",
+      link: "https://www.edx.org/learn/shakespeare/harvard-university-shakespeare-s-life-and-work"
+    }],
+    "Free": [{
+      name: "Introduction to Philosophy",
+      provider: "Udemy",
+      duration: "1 hour",
+      discription: "Explore age-old philosophical questions about existence, knowledge, morality, and the mind. Cover thinkers like Plato, Descartes, and Kant while developing your own critical reasoning. Great for beginners and anyone who loves deep thinking.",
+      image: "https://philosophyawayoflife.com/wp-content/uploads/2021/10/philosophy-a-way-of-life-ph.jpg?w=1000",
+      link: "https://www.udemy.com/course/introduction-to-philosophy-with-plato/"
+    },
+    {
+      name: "Film, Images & Historical Interpretation",
+      provider: "Udemy",
+      duration: "30 min",
+      discription: "Understand how visual media—film, photography, art—shape and distort our understanding of historical events. The course blends media analysis with historical critique and is ideal for students of history, journalism, and media studies.",
+      image: "https://www.thoughtco.com/thmb/LSSFSX6XnWWvyJieTJ_Vk_INrqE=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/GettyImages-654313960-5c532556c9e77c00014b0273.jpg",
+      link: "https://www.udemy.com/course/filmschoolcinema101/"
+    }]
+  },
+  "Environmental Studies": {
+    "Paid": [
+    {
+      name: "The Science of Climate Change",
+      provider: "EDX",
+      duration: "16 hours",
+      discription: "Understand the evidence, causes, and consequences of climate change through a scientific lens. This course covers carbon cycles, greenhouse gases, and global warming's impact on ecosystems and society. Ideal for students, educators, and policy advocates.",
+      image: "https://d2jx2rerrg6sh3.cloudfront.net/image-handler/ts/20211112073341/ri/673/picture/2021/11/shutterstock_1127223938.jpg",
+      link: "https://www.edx.org/learn/climate-change/sdg-academy-climate-change-the-science-and-global-impact"
+    },
+    {
+      name: "Biodiversity and Conservation",
+      provider: "EDX",
+      duration: "7 hours",
+      discription: "Learn about Earth's incredible biodiversity, the threats it faces, and conservation strategies to protect it. This course combines ecology, biology, and policy. Ideal for aspiring environmentalists, biologists, and nature lovers.",
+      image: "https://www.euroschoolindia.com/wp-content/uploads/2024/02/What-is-Biodiversity-Importance-Ways-To-Conserve.jpg",
+      link: "https://www.edx.org/learn/ecosystems/ethx-environmental-dna-sensing-the-diversity-of-life-and-assessing-ecosystem-health"
+    }],
+    "Free": [{
+      name: "Introduction to Environmental Law and Policy",
+      provider: "EDX",
+      duration: "5 hours",
+      discription: "Explore how laws and regulations influence environmental protection, pollution control, and natural resource management. The course features U.S. and global case studies. Great for students of law, policy, or sustainability.",
+      image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRL35uKzT0OBe8ifg11BKOLwIn--kZi3ShLBw&s",
+      link: "https://www.edx.org/learn/climate-change/massachusetts-institute-of-technology-cities-and-climate-change-mitigation-and-adaptation"
+    },
+    {
+      name: "Global Environmental Management",
+      provider: "FutureLearn",
+      duration: "4 hours",
+      discription: "Gain insights into managing environmental challenges at a global scale. Topics include sustainable development, environmental economics, waste management, and international agreements. Ideal for professionals or students in environmental science and policy.",
+      image: "https://media.istockphoto.com/id/1413930025/photo/crystal-globe-putting-on-moss-esg-icon-for-environment-social-and-governance.jpg?s=612x612&w=0&k=20&c=AfK7HKFtO8yy6JE557Agw6GJXojjMJWlH2lrGM_RD2o=",
+      link: "https://www.futurelearn.com/courses/introduction-to-environmental-science"
+    }]
+  }
+}
+
+function Ram(event) {
+  let filterer = document.getElementById("Filter").value;
+  let categ = document.getElementById("Category").value
+  
+  let resultDiv = document.getElementById("result")
+  let course = []
+  if (categ === "Choose from here" && filterer === "Select from here") {
+    alert("कृपया Category और Filter दोनों में से कम से कम कोई एक चुनें");
+    localStorage.removeItem(r)
+    return; // यहीं function रुक जाएगा
+    //  console.log(course)
+  }
+  
+  else if (categ !== "Choose from here" && filterer === "Select from here") {
+    course = courseData[categ]
+    
+    console.log(course)
+    
+  } else if (categ !== `Choose from here` && filterer !== `Select from here`) {
+    course = courseData[categ][filterer]
+    console.log(course)
+  }
+  
+  else if (categ === `Choose from here` && filterer !== `Select from here`) {
+    for (let cate in courseData) {
+      let z
+      
+      let y = courseData[cate]
+      if (y[filterer]) {
+        
+        z = y[filterer]
+        
+      }
+      course.push(z)
+      
+      
+    }
+    
+  }
+  
+  // let u=  JSON.stringify(Object.values(course).flat())
+  //if (window.onload) {
+  show(Object.values(course).flat())
+  //}
+  
+}
+
+function show(course) {
+  let r = "";
+  
+  let resultDiv = document.getElementById("result")
+  
+  if (course.length >= 1) {
+    for (let i = 0; i < course.length; i++) {
+      let item = course[i]
+      r += ` <div class="card">
+  
+    <div class="img">
+      <img src="${item.image}" alt="${item.name}"/>  
+    </div>
+    <div class="content">
+      <h2>${item.name}</h2>  
+      <p><span>Provider:</span> ${item.provider}</p>  
+      <p><span>Duration:</span> ${item.duration}</p>  
+
+
+  
+    <p><span>Description:</span> ${item.discription}</p>  
+    <a href="${item.link}" target="_blank">Go to course</a>  
+  </div>
+</div>`
+    }
+    
+    
+    
+    
+    
+    
+  } else {
+    r = `<button type="reset" onclick="reset()"> try again    </button>`
+    reset()
+  }
+  
+  localStorage.setItem("r", r)
+  window.location.href = "Show.html"
+  // o.html पर data दिखाने के लिए — ये नीचे की 3 lines जोड़ो
+  //if (document.getElementById("result")) {
+  //resultDiv.innerHTML = localStorage.getItem("r");
+  //}
+  
+}
+//Load result on o.html
+function loadResult() {
+  let r = localStorage.getItem("r");
+  if (r) {
+    document.getElementById("result").innerHTML = r;
+  }
+}
+
+function reset(event) {
+  
+  document.getElementById("Category").value = "Choose from here";
+  document.getElementById("Filter").value = "Select from here";
+  document.getElementById("result").innerHTML = ""; // Clear results
+  
+  console.log(course)
+}
